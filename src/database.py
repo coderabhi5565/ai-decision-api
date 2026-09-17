5aa42331935dc1e22b722b4006819bf017bcba92
coderabhi5565 <gabhinav084@gmail.com>
2026-09-17 15:10:59 +0530

    Make database configuration deployment ready

--- src/database.py
+++ src/database.py
@@ -1,23 +1,24 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
+load_dotenv()
 
-DATABASE_URL = "sqlite:///./decision_api.db"
-
-
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./decision_api.db"
+)
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
-
-
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
-
-
 class Base(DeclarativeBase):
     pass
 
